84ffcbd283a2441a2c5574770e73476e362ece00
emmanuel.ratier@epitech.eu <emmanuel.ratier@epitech.eu@epitech.eu>
2018-12-07 19:54:08 +0100

    router fonctionnel

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App" style={{height: '1300px', position: 'relative'}}>
             <Layout fixedHeader>
-                <Header title={<span><span style={{ color: '#ddd' }}>Area / </span><strong>The Title</strong></span>}>
+                <Header title={<span><span style={{ color: '#ddd' }}>portfolio </span><strong>Emmanuel Ratier</strong></span>}>
                     <Navigation>
-                        <a href="/">Link</a>
-                        <a href="/">Link</a>
-                        <a href="/">Link</a>
-                        <a href="/">Link</a>
+                        <Link to="/resume">Resume</Link>
+                        <Link to="/aboutme">About me</Link>
+                        <Link to="/projects">Projects</Link>
+                        <Link to="/contact">Contact</Link>
                     </Navigation>
                 </Header>
                 <Drawer title="Title">
                     <Navigation>
-                        <a href="/">Link</a>
-                        <a href="/">Link</a>
-                        <a href="/">Link</a>
-                        <a href="/">Link</a>
+                        <Link to="/resume">Resume</Link>
+                        <Link to="/aboutme">About me</Link>
+                        <Link to="/projects">Projects</Link>
+                        <Link to="/contact">Contact</Link>
                     </Navigation>
                 </Drawer>
+                <Main />
                 <Content />
             </Layout>
       </div>
